0cdd0c26f93b37e08449f18827f3f61034c7ea81
Stefan <decimelli@ibm.com>
2024-01-30 15:22:59 -0500

    add nullable

--- api/lib/errorHandlers.ts
+++ api/lib/errorHandlers.ts
@@ -2,9 +2,7 @@ import type { PrismaClientKnownRequestError, PrismaClientValidationError } from 
 
 export type PrismaError = PrismaClientKnownRequestError | PrismaClientValidationError
 
-export function formatPrismaError(
-	e: PrismaError
-): { error: string[] } {
+export function formatPrismaError(e: PrismaError): { error: string[] } {
 	return {
 		error: e.message.trim().split("\n").splice(1).filter(m => m !== "")
 	};
